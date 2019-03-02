6ed019d4bb972b572c0487de1cb5ee49e926c4c2
Angelina <brubow@newvisionsproject.de>
2019-03-02 11:36:00 +0100

    initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1D-Arrays
+            int[] intergerListe = new int[4];
+
+            intergerListe[0] = 10;
+            intergerListe[1] = 20;
+            intergerListe[2] = 30;
+            intergerListe[3] = 40;
+
+            Console.WriteLine(intergerListe[2]);
+            Console.ReadKey();
+            
+        }
+    }
+}
